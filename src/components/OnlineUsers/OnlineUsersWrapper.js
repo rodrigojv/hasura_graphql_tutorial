39c6118ae0de7f1288249efde43882e76556e6c7
Rodrigo Villalba <rodris.jvo@gmail.com>
2019-06-04 16:35:43 -0400

    Update last seen of my user

--- src/components/OnlineUsers/OnlineUsersWrapper.js
+++ src/components/OnlineUsers/OnlineUsersWrapper.js
@@ -1,28 +1,44 @@
 import React, { Component } from "react";
-
+import { withApollo } from "react-apollo";
 import OnlineUser from "./OnlineUser";
+import gql from "graphql-tag";
+
+const UPDATE_LAST_SEEN = gql`
+  mutation updateLastSeen($now: timestamptz!) {
+    update_users(where: {}, _set: { last_seen: $now }) {
+      affected_rows
+    }
+  }
+`;
 
 class OnlineUsersWrapper extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
+    this.client = props.client;
 
     this.state = {
-      onlineUsers: [
-        { name: "someUser1" },
-        { name: "someUser2" }
-      ]
+      onlineUsers: [{ name: "someUser1" }, { name: "someUser2" }]
     };
+  }
+
+  updateLastSeen() {
+    this.client.mutate({
+      mutation: UPDATE_LAST_SEEN,
+      variables: { now: new Date().toISOString() }
+    });
+  }
+
+  componentDidMount() {
+    setInterval(() => this.updateLastSeen(), 30000);
   }
 
   render() {
     const onlineUsersList = [];
     this.state.onlineUsers.forEach((user, index) => {
       onlineUsersList.push(
-        <OnlineUser
-          key={index}
-          index={index}
-          user={user}
-        />);
+        <OnlineUser key={index} index={index} user={user} />
+      );
     });
 
     return (
@@ -31,10 +47,10 @@ class OnlineUsersWrapper extends Component {
           Online users - {this.state.onlineUsers.length}
         </div>
 
-        { onlineUsersList }
+        {onlineUsersList}
       </div>
     );
   }
 }
 
-export default OnlineUsersWrapper;
+export default withApollo(OnlineUsersWrapper);
